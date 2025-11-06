0884168daae1cc0eb41f4b7a82c9c109e1ddf2fb
alif <shahrierislamalif@gmail.com>
2025-11-06 19:36:20 +0600

    Refactors movie ID parsing; removes unused file
    Extracts the logic for reading and parsing the ID parameter from request paths into a dedicated `readIDParam` helper. This centralizes common parameter handling, improving consistency and reducing duplication across handlers.

--- cmd/api/movies.go
+++ cmd/api/movies.go
@@ -3,9 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
-
-	"github.com/julienschmidt/httprouter"
+	
 )
 
 func (app *application) createMovieHandler(w http.ResponseWriter , r *http.Request){
@@ -13,9 +11,7 @@ func (app *application) createMovieHandler(w http.ResponseWriter , r *http.Reque
 }
 
 func (app *application) showMovieHandler(w http.ResponseWriter , r *http.Request){
-	params := httprouter.ParamsFromContext(r.Context())
-
-	id , err :=	strconv.ParseInt(params.ByName("id") , 10 , 64)
+	id  , err := app.readIDParam(r)
 
 	if err != nil {
 		http.NotFound(w , r)

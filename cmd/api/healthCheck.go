1f8682a02470bd3672e1da89462a33fa068294af
alif <shahrierislamalif@gmail.com>
2025-11-13 10:09:49 +0600

    Refactor healthcheck response to use structured JSON and update writeJSON function for improved error handling

--- cmd/api/healthCheck.go
+++ cmd/api/healthCheck.go
@@ -1,16 +1,30 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter , r *http.Request){
+
+	/*
+	Fixed string format of the response body , this is useful for fast response for smaller body
+
 	js := `{"status" : "available" , "environment" : "%s" , "version" : "%s" }`
 	js = fmt.Sprintf(js , app.config.env , version)
 
-	w.Header().Set("Content-Type" , "application/json")
+	*/
 
-	w.Write([]byte(js))
+	data := map[string]string{
+		"status" : "available",
+		"environment" : app.config.env,
+		"version" : version,
+	}
+
+	err := app.writeJSON(w , http.StatusOK , data , nil)
+
+	if err != nil {
+		app.logger.Error(err.Error())
+		http.Error(w , "The server encountered a problem and could not process your request" , http.StatusInternalServerError)
+	}
 
 }
--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"strconv"
@@ -18,5 +19,29 @@ func (app *application) readIDParam(r *http.Request) (int64 , error){
 	}
 
 	return id , nil
+
+}
+
+func (app *application) writeJSON(w http.ResponseWriter , status int , data any , headers http.Header) error{
+	js , err := json.Marshal(data)
+
+	if err != nil {
+		http.Error(w , err.Error() , http.StatusInternalServerError)
+		return err
+	}
+
+	// js = append(js, '\n') --> You use append to add a new byte to the end of the slice . As the js is byte array , we've to append byte by byte to it
+	js = append(js , "\n"...) // --> If you want to append a string to a byte array , you have to append it as a slice of bytes by destructuring it
+
+	// If we figure out that there's not going to be anymore error after a certain point then it's time to write headers
+	for key , val := range headers{
+		w.Header()[key] = val
+	}
+
+	w.Header().Set("Content-Type" , "application/json")
+	w.WriteHeader(status)
+	w.Write(js)
+
+	return nil // if everything goes well then we return nil
 
 }

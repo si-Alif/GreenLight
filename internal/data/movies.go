3c50538be602160ed155bc2a1abb86d1ae341a78
alif <shahrierislamalif@gmail.com>
2025-11-14 11:59:41 +0600

    modifying movies struct's json output to use camel case

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -3,12 +3,14 @@ package data
 import "time"
 
 // for holding all the relevant data about a movie
+
+// next step : modified movie struct to show snake case when the appear as a response
 type Movie struct {
-	ID int64
-	CreatedAt time.Time
-	Title string
-	Year int32
-	Runtime int32 // to store the runtime in minutes
-	Genres []string
-	Version int32 //starts with 1 and then gets incremented each time the movie info is updated
+	ID int64 `json:"id"`
+	CreatedAt time.Time `json:"-"` // used - directive so that this field is omitted from the response body
+	Title string `json:"title"`
+	Year int32 `json:"year,omitzero"`
+	Runtime int32  `json:"runtime,omitzero"`      // to store the runtime in minutes
+	Genres []string `json:"genres,omitzero"`
+	Version int32  `json:"version"`   //starts with 1 and then gets incremented each time the movie info is updated
 }

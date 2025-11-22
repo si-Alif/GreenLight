fe2085df0d76f8782b6b0ae8fcfa68d23a6566f5
alif <shahrierislamalif@gmail.com>
2025-11-22 20:29:56 +0600

    Add database connection configuration flags for max open , idle connections and idle timeout for a connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -21,6 +21,9 @@ type config struct {
 	env string // development or production or test
 	db struct{
 		dsn string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime time.Duration
 	}
 
 }
@@ -49,7 +52,11 @@ func main(){
 
 	env := flag.String("env" , "development" , "Environment (development | production | test)")
 
+	// flags for db configuration (postgrSQL specific)
 	flag.StringVar(&cfg.db.dsn , "db-dsn" , os.Getenv("GREENLIGHT_DB_DSN") , "PostgreSQL DSN")
+	flag.IntVar(&cfg.db.maxOpenConns , "db-max-open-conns" , 25 , "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns , "db-max-idle-conns" , 25 , "PostgreSQL max idle connections")
+	flag.DurationVar(&cfg.db.maxIdleTime , "db-max-idle-time" , 15 * time.Minute , "PostgreSQL max idle time for a connection")
 
 	flag.Parse()
 
@@ -97,6 +104,10 @@ func openDB(cfg config) (*sql.DB , error){
 	if err != nil {
 		return nil , err
 	}
+
+	db.SetMaxOpenConns(cfg.db.maxOpenConns)
+	db.SetMaxIdleConns(cfg.db.maxIdleConns)
+	db.SetConnMaxIdleTime(cfg.db.maxIdleTime)
 
 	ctx , cancel := context.WithTimeout(context.Background() , 5 * time.Second)
 	defer cancel()

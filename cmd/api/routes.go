79398fad802bdb0a28338925fd5bec750a88541d
alif <shahrierislamalif@gmail.com>
2026-01-19 22:18:51 +0600

    Adds metrics endpoint for expvar to expose runtime metrics

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"expvar"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
@@ -38,6 +39,9 @@ func (app *application) routes() http.Handler{
 
 	// user token
 	router.HandlerFunc(http.MethodPost , "/v1/tokens/authentication" , app.createAuthenticationTokenHandler)
+
+	// Metrics
+	router.Handler(http.MethodGet , "/debug/vars" , expvar.Handler())
 
 	return app.recoverPanic(app.enableCORS(app.rateLimit(app.authenticate(router)))) // Wrap the router with the panic recovery middleware
 
